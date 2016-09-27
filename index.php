7dd092aa7a87518b13699458d4c4d107e0dee039
Art4 <art4@wlabs.de>
2016-09-27 14:26:32 +0200

    Update to Bootstrap 4, install FontAwesome 4.6

--- index.php
+++ index.php
@@ -1,13 +1,18 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
 	<title>Full Youtube RSS-Feed</title>
-	<link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<link rel="stylesheet" href="vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
+	<link rel="stylesheet" href="vendor/fortawesome/font-awesome/css/font-awesome.min.css">
 </head>
 
 <body>
+<div class="container">
 	<h1>Full Youtube RSS-Feed</h1>
-	<p>Subscribe a RSS feed from a Youtube channel with full Videodownload.</p>
+	<p>Subscribe a RSS feed from a Youtube channel with full video download.</p>
 <?php
 
 error_reporting(0);
@@ -26,20 +31,28 @@ if ( isset($_GET['channel_url']) )
 
 	$channel = preg_replace('/[^A-Za-z0-9\-]/', '', $channel);
 
-	$url = './rss.php?channel_id=' . $channel;
+	$url = '.';
+
+	if ( isset($_SERVER['SCRIPT_URL']) )
+	{
+		$url = rtrim($_SERVER['SCRIPT_URL'], '/');
+	}
+
+	$url .= '/rss.php?channel_id=' . $channel;
 ?>
-<div class="jumbotron">
-	<p><a href="<?php echo $url; ?>">RSS-Feed</a></p>
+<div class="alert alert-success">
+	<p><a href="<?php echo $url; ?>" class="alert-link"><i class="fa fa-rss" aria-hidden="true"></i> RSS-Feed</a></p>
 </div>
 <?php
 }
 ?>
 <form method="get" action="./">
 	<div class="form-group">
-		<label for="channel_url">Channel url:</label>
+		<label for="channel_url">Channel-ID or channel url:</label>
 		<input type="text" class="form-control" id="channel_url" name="channel_url" placeholder="https://youtube.com/channel/UC...">
 	</div>
 	<button type="submit" class="btn btn-default">Submit</button>
 </form>
+</div>
 </body>
 </html>
